b858a6182e622d771363b92982bdc4f13b01b33c
Anna <58006989+Anna-Z-Krk@users.noreply.github.com>
2022-06-26 06:22:09 +0200

    RWD added, partly solved task with JS
    Changes in media targets (add breakpoints) and solves JavaScript button clicking - all works as it should be. Not solved the problem of resetting clicking count and refreshing the page (session / local storage). Not checked with validator of W3C yet.

--- src/js/main.js
+++ src/js/main.js
@@ -4,32 +4,38 @@ const closingX = document.querySelector(".modal-alert__btn");
 const popupAlertArea = document.querySelector(".modal-alert__wrapper");
 const popup = document.querySelector(".popup");
 const clicks = document.querySelector(".modal-alert__clicks");
+const resetBtn = document.querySelector(".modal-alert__btn-reset");
 let countedClicks = 0;
 
-btn.addEventListener("click", function () {
-  popupAlert.style.display = "flex";
-});
-
-closingX.addEventListener("click", function () {
-  popupAlert.style.display = "none";
-});
+const countingClicks = () => {
+  countedClicks += 1;
+  console.log(countedClicks);
+  clicks.innerHTML = `${countedClicks} times`;
+  if (countedClicks > 5) {
+    resetBtn.style.display = "flex";
+  } else {
+    resetBtn.style.display = "none";
+  }
+};
 
 const handleClick = (e) => {
   if (e.target.classList != "popup")
     return (popupAlert.style.display = "none"), console.log(e.target);
   else popupAlert.style.display = "flex";
 };
+
+// btn.addEventListener("click", countingClicks);
+
+btn.addEventListener("click", function () {
+  popupAlert.style.display = "flex";
+  countingClicks();
+});
+
+closingX.addEventListener("click", function () {
+  popupAlert.style.display = "none";
+});
 popupAlert.addEventListener("click", handleClick);
 
-// No added function to reset amount of clicks with additional button, temporarly it's just reseting the value
-btn.addEventListener(
-  "click",
-  (countringClicks = () => {
-    countedClicks += 1;
-    console.log(countedClicks);
-    clicks.innerHTML = `${countedClicks} times`;
-    if (countedClicks > 5) {
-      clicks.innerHTML = `${(countedClicks = 1)} times`;
-    }
-  })
-);
+resetBtn.addEventListener("click", function () {
+  clicks.innerHTML = `${(countedClicks = 0)} times`;
+});
