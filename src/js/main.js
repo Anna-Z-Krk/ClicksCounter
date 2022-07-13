680dd855fd37f5db6ec22ee78457a5d836ed9bc6
Anna <58006989+Anna-Z-Krk@users.noreply.github.com>
2022-07-13 05:16:49 +0200

    Local storage saves clicks
    Now local storage of browser saves clicks. You can refresh browser, and you will not lose amount of clicks. Button reset clean local storage and restart counting clicks.

--- src/js/main.js
+++ src/js/main.js
@@ -5,13 +5,20 @@ const popupAlertArea = document.querySelector(".modal-alert__wrapper");
 const popup = document.querySelector(".popup");
 const clicks = document.querySelector(".modal-alert__clicks");
 const resetBtn = document.querySelector(".modal-alert__btn-reset");
-let countedClicks = 0;
-
+// let countedClicks = 0;
+let countedClicks = parseInt(localStorage.getItem("btnClicksSaved") ?? "0");
 const countingClicks = () => {
   countedClicks += 1;
-  console.log(countedClicks);
-  clicks.innerHTML = `${countedClicks} times`;
-  if (countedClicks > 5) {
+
+  localStorage.setItem("btnClicksSaved", countedClicks);
+  let savedClicks = localStorage.getItem("btnClicksSaved");
+
+  clicks.innerHTML = `${savedClicks} times`;
+
+  console.log(countedClicks + " counted");
+  console.log(savedClicks + " local storage");
+
+  if (savedClicks > 5) {
     resetBtn.style.display = "flex";
   } else {
     resetBtn.style.display = "none";
@@ -19,12 +26,9 @@ const countingClicks = () => {
 };
 
 const handleClick = (e) => {
-  if (e.target.classList != "popup")
-    return (popupAlert.style.display = "none"), console.log(e.target);
+  if (e.target.classList != "popup") return (popupAlert.style.display = "none");
   else popupAlert.style.display = "flex";
 };
-
-// btn.addEventListener("click", countingClicks);
 
 btn.addEventListener("click", function () {
   popupAlert.style.display = "flex";
@@ -34,8 +38,11 @@ btn.addEventListener("click", function () {
 closingX.addEventListener("click", function () {
   popupAlert.style.display = "none";
 });
+
 popupAlert.addEventListener("click", handleClick);
 
 resetBtn.addEventListener("click", function () {
   clicks.innerHTML = `${(countedClicks = 0)} times`;
+  localStorage.clear();
+  // console.log(countedClicks + "reset counted");
 });
